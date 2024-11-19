d8690521b9f869f7ff96aee5ae6be457a98249b4
vtw <vtiaraw2@gmail.com>
2024-11-19 13:09:14 +0700

    feat: add currency formatter

--- app/src/main/java/com/dicoding/picodiploma/productdetail/Helper.kt
+++ app/src/main/java/com/dicoding/picodiploma/productdetail/Helper.kt
@@ -6,14 +6,14 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun String.withNumberingFormat(): String {
-    return NumberFormat.getNumberInstance().format(this.toDouble())
-}
-
 fun String.withDateFormat(): String {
     val format = SimpleDateFormat("dd/MM/yyyy", Locale.US)
     val date = format.parse(this) as Date
     return DateFormat.getDateInstance(DateFormat.FULL).format(date)
+}
+
+fun String.withNumberingFormat(): String {
+    return NumberFormat.getNumberInstance().format(this.toDouble())
 }
 
 fun String.withCurrencyFormat(): String {
