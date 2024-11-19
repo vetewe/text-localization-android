5c33526d531502e5a2cc45a5ceb1192e451d0156
vtw <vtiaraw2@gmail.com>
2024-11-19 12:55:29 +0700

    fix: setupdata function

--- app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -10,7 +10,6 @@ import android.view.WindowManager
 import com.dicoding.picodiploma.productdetail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +28,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
     private fun setupData() {
-        val data = RemoteDataSource(this)
+        val repository = RemoteDataSource(this)
         val product = repository.getDetailProduct().apply {
             binding.apply {
                 previewImageView.setImageResource(image)
@@ -44,19 +56,5 @@ class MainActivity : AppCompatActivity() {
                 ratingTextView.text = getString(R.string.ratingFormat, rating, countRating)
             }
         }
-    }
-
-
-    private fun setupView() {
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
-        supportActionBar?.hide()
     }
 }
